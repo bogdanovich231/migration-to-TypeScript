adf54c91a0329bb08fbbe413af1e4562b63a4fc2
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2023-06-11 20:34:15 +0200

    feat: create Api key

--- migration_to_TypeScript/src/components/controller/appLoader.ts
+++ migration_to_TypeScript/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'https://newsapi.org/v2/everything?domains=wsj.com&apiKey=061aca91965445358b93ce15d016e915',
         });
     }
 }
