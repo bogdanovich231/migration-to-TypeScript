1dd70ca318b451fc5b84d5825065fd7b282a30eb
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2023-06-11 20:19:11 +0200

    feat: migration app to TS

--- migration_to_TypeScript/src/components/app/app.ts
+++ migration_to_TypeScript/src/components/app/app.ts
@@ -1,17 +1,26 @@
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
 
+interface newsData {
+
+}
+
+interface sourceData {
+
+}
 class App {
+    private controller: AppController;
+    private view: AppView;
+
     constructor() {
         this.controller = new AppController();
         this.view = new AppView();
     }
 
-    start() {
-        document
-            .querySelector('.sources')
-            .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
-        this.controller.getSources((data) => this.view.drawSources(data));
+    public start(): void {
+        document.querySelector('.sources');
+        document.addEventListener('click', (e: Event) => this.controller.getNews(e, (data: newsData) => this.view.drawNews(data)));
+        this.controller.getSources((data: sourceData) => this.view.drawSources(data));
     }
 }
 
